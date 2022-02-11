27fe2f72620f3b43d4cb7d9ac92facafa72da0ea
wilsona <jowill836@gmail.com>
2022-02-11 11:59:20 +0100

    version termine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { TodolistService, TodoItem,} from './todolist.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'l3m-tpX-todolist-angular-y2022';
+
+  constructor(private TDLS : TodolistService){
+    
+    
+  }
+
+  get obs(){
+    return this.TDLS.observable;
+  }
+
+  create(...labels: readonly string[]){
+    this.TDLS.create(...labels);
+  }
+
+  delete(...items: readonly TodoItem[]){
+    this.TDLS.delete(...items);
+  }
+ 
+  update(data: Partial<TodoItem>, ...items: readonly TodoItem[]){
+    this.TDLS.update(data,...items);
+  }
+
+  trackById(n : number, item : TodoItem): number{
+    return item.id;
+  }
+
+
 }
